de9c805c3541f7ce45ec64a98ff5c42c1d38d3b3
AlekssGu <gusevs.aleksandrs@gmail.com>
2016-11-02 22:24:34 +0200

    Minor changes to layout

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 $(document).ready(function(){
-     $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
-        // scroll body to 0px on click
-        $('#back-to-top').click(function () {
-            $('#back-to-top').tooltip('hide');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
 
-        $('#back-to-top').tooltip('show');
+    $('#back-to-top').tooltip('show');
+
+    if (consoleOpened)
+        console.warn("Perhaps you are an employer who is looking for talents?\n" +
+                     "If so - please leave me a message on Facebook or LinkedIn");
 });
+
+function consoleOpened () {
+    console.profile();
+    console.profileEnd();
+    if(console.clear) { console.clear() };
+    return console.profiles.length > 0;
+}
